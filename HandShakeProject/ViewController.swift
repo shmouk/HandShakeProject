e19d1295d5155903e3f9eec5858168e98408fb01
shmouk <maretski.mark@gmail.com>
2023-07-14 17:09:21 +0300

    create

---
+++ HandShakeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HandShakeProject
+//
+//  Created by Марк on 14.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
